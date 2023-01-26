0ee5b5ef2e651b696f148df8c2d3bcd757e97e40
Vishwas <VishwasDubey150@gmail.com>
2023-01-27 01:44:30 +0530

    sign up page created

--- app/src/main/java/com/example/projmanager/MainActivity.kt
+++ app/src/main/java/com/example/projmanager/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.projmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+    }
+
+    fun signup_page(view: View) {
+        val intent=Intent(this,Signup::class.java)
+        startActivity(intent)
     }
 }
